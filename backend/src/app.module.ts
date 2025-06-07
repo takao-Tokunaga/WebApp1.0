a2bf778707c1531671bd7062159c0013aaa1a330
Kotaro-coder <k.inoue1071@email.com>
2025-06-07 11:19:27 +0900

    chore: 改行コードなど軽微な変更を整理するため

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { TaskModule } from './task/task.module'; 
+import { TaskModule } from './task/task.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
