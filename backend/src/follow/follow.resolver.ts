be5377e7bbb8226ea944dfb4eaa3ee247199d67c
michel <tokunagao749@gmail.com>
2025-06-18 13:51:56 +0900

    作業途中

--- backend/src/follow/follow.resolver.ts
+++ backend/src/follow/follow.resolver.ts
@@ -1,9 +1,8 @@
 import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateTaskInput } from './dto/createTask.input';
-import { Task as TaskModel } from './models/task.model';
+import { Follow as FollowModel } from './models/follow.model';
 import { FollowService } from './follow.service';
 import { Follow } from '@prisma/client';
-import { UpdateTaskInput } from './dto/updateTask.input';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { UseGuards } from '@nestjs/common';
 
@@ -11,33 +10,32 @@ import { UseGuards } from '@nestjs/common';
 export class FollowResolver {
   constructor(private readonly followService: FollowService) {}
 
-  @Query(() => [FollowModel], { nullable: 'items' })
+  @Mutation(() => Boolean)
   @UseGuards(JwtAuthGuard)
-  async getTasks(
-    @Args('userId', { type: () => Int }) userId: number,
-  ): Promise<Task[]> {
-    return await this.taskService.getTasks(userId);
+  async follow(
+    @Args('followerId', { type: () => Int }) followerId: number,
+    @Args('folloingId', { type: () => Int }) followingId: number,
+  ): Promise<boolean> {
+    await this.followService.follow(followerId, followingId);
+    return true;
   }
 
-  @Mutation(() => TaskModel)
+  @Mutation(() => Boolean)
   @UseGuards(JwtAuthGuard)
-  async createTask(
-    @Args('createTaskInput') createTaskInput: CreateTaskInput,
-  ): Promise<Task> {
-    return await this.taskService.createTask(createTaskInput);
+  async unfollow(
+    @Args('followerId', { type: () => Int }) followerId: number,
+    @Args('followingId', { type: () => Int }) followingId: number,
+  ): Promise<boolean> {
+    await this.followService.unfollow(followerId, followingId);
+    return true;
   }
 
-  @Mutation(() => TaskModel)
+  @Query(() => [Int])
   @UseGuards(JwtAuthGuard)
-  async updateTask(
-    @Args('updateTaskInput') updateTaskInput: UpdateTaskInput,
-  ): Promise<Task> {
-    return await this.taskService.updateTask(updateTaskInput);
-  }
+  async getFollowingIds(
+    @Args('followingId', { type: () => Int }) followingId: number,
+  ): Promise<number[]> {
+    await this.followService.follow(followerId)
 
-  @Mutation(() => TaskModel)
-  @UseGuards(JwtAuthGuard)
-  async deleteTask(@Args('id', { type: () => Int }) id: number): Promise<Task> {
-    return await this.taskService.deleteTask(id);
   }
 }
--- backend/src/follow/follow.service.ts
+++ backend/src/follow/follow.service.ts
@@ -6,9 +6,34 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class FollowService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async createFollow(followerId: number, followingId: number): Promise<Follow> {
+  async follow(followerId: number, followingId: number): Promise<Follow> {
     return await this.prismaService.follow.create({
       data: { followerId, followingId },
+    });
+  }
+
+  async unfollow(followerId: number, followingId: number): Promise<Follow> {
+    return this.prismaService.follow.delete({
+      where: {
+        followerId_followingId: {
+          followerId,
+          followingId,
+        },
+      },
+    });
+  }
+
+  async getFollowing(userId: number) {
+    return this.prismaService.follow.findMany({
+      where: { followingId: userId },
+      include: { following: true },
+    });
+  }
+
+  async getFollowers(userId: number) {
+    return this.prismaService.follow.findMany({
+      where: { followerId: userId },
+      include: { follower: true },
     });
   }
 }
